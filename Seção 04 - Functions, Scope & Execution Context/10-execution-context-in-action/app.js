5dd03c0ee6ce4a01556cec1b156b8eeb50b35945
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2023-04-17 13:21:11 -0300

    Seção 04 - Finalizada

---
+++ Seção 04 - Functions, Scope & Execution Context/10-execution-context-in-action/app.js
@@ -0,0 +1,12 @@
+const x =  100;
+const y = 50;
+
+function getSum(n1, n2) {
+    const sum = n1 + n2;
+    return sum;
+};
+
+const sum1 = getSum(x, y);
+const sum2 = getSum(10,5);
+
+console.log(sum1, sum2);
---
+++ Seção 04 - Functions, Scope & Execution Context/11-call-stack/app.js
@@ -0,0 +1,35 @@
+// First Example
+
+// function first() {
+//     console.log('first...');
+// };
+
+// function second() {
+//     console.log('second...');
+// };   
+
+// function third() {
+//     console.log('third...');
+// };
+
+// first();
+// second();
+// third();
+
+// Second Example
+
+function first() {
+    console.log('first...');
+    second();
+};
+
+function second() {
+    console.log('second...');
+    third();
+};    
+
+function third() {
+    console.log('third...');
+};
+
+first();
