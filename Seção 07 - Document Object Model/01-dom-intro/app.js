4ba3c59a8a535ab2599de4c80765c13f2784346a
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2023-04-27 13:46:33 -0300

    Seção 07 - Parte 01

---
+++ Seção 07 - Document Object Model/01-dom-intro/app.js
@@ -0,0 +1,18 @@
+// console.log(window);
+console.dir(window.document);
+
+console.log(document.body.innerHTML);
+
+console.log(document.links);
+console.log(document.links[0]);
+
+//document.body.innerHTML = '<h1>Hello World!</h1>';
+
+// document.write('Hello from JS');
+
+console.log(document.getElementById('main'));
+
+const main = document.getElementById('main');
+
+// main.innerHTML = '<h1>Hello from main</h1>';
+
